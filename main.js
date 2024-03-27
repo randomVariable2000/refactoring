34b3b68710700810f95d6147cb8c406e826c7df1
randomVariable2000 <1597888126@qq.com>
2024-03-27 15:13:35 +0800

    feat: support htmlStatement

--- main.js
+++ main.js
@@ -1,11 +1,16 @@
 import './style.css';
-import { statement } from './statement.js';
+import { statement, htmlStatement } from './statement.js';
 import plays from './plays.json';
 import invoices from './invoices.json';
 
 document.querySelector('#app').innerHTML = `
+  <h1 style="font-style: italic;">string statement</h1>
   <div style="white-space: pre-wrap">
   ${statement(invoices[0], plays)}
   </div>
+  <hr/>
+
+  <h1 style="font-style: italic;">html statement</h1>
+  ${htmlStatement(invoices[0], plays)}
 `;
 
